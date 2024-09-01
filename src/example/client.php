414e7d1a9fc5df7b214087926de592657d5f8257
LAGGOUNE Walid <walidlaggoune159@gmail.com>
2024-09-01 20:06:18 +0100

    Updated

--- src/example/client.php
+++ src/example/client.php
@@ -9,7 +9,7 @@ run(function () {
     if (! $client->connect('0.0.0.0', 9505, 0.5)) {
         echo "connect failed. Error: {$client->errCode}\n";
     }
-    $data = ['appId' => 'your_app_id_here', 'clientId' => '22f8e456-93f2-4173-8f2d-8a010abcceb1', 'data' => ['type' => 'Point', 'coordinates' => [1, 1]]];
+    $data = ['appId' => '123', 'clientId' => '22f8e456-93f2-4173-8f2d-8a010abcceb1', 'data' => ['type' => 'Point', 'coordinates' => [1, 1]]];
     $data = msgpack_pack($data);
     $client->send($data);
     $client->close();
